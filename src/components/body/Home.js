ed93a3a0472b693d72393933d28561d519df7f04
Klifford Agujar <forddagujar95@gmail.com>
2020-08-26 00:57:44 -0400

    new files

---
+++ src/components/body/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom'
+
+
+function Home() {
+    const history = useHistory();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
