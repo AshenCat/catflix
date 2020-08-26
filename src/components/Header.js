d71185bb8732f6c6ae03d343c1b58049ed260305
Klifford Agujar <forddagujar95@gmail.com>
2020-08-26 00:57:30 -0400

    added user button in the appbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,86 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Button, Typography, Menu, MenuItem } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon  from '@material-ui/icons/Menu'
+import AccountCircle  from '@material-ui/icons/AccountCircle'
+import { withRouter } from 'react-router-dom';
 
-export default function Header() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+function Header(props) {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const goToLogin = () => {
+        props.history.push('/Login')
+    }
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-            </Toolbar>
-        </AppBar>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        
+                    </Typography>
+                    {props.session ? 
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit">
+                                <Typography className={classes.menuButton}>
+                                    {props.session.username}
+                                </Typography>
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                  vertical: 'top',
+                                  horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                  vertical: 'top',
+                                  horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}>
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                         :
+                    <Button color="inherit" onClick={goToLogin}>Login</Button>
+                    }
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
+
+export default withRouter(Header)
