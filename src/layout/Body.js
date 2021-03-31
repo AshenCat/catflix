04f423baa622718a41fedf1a764c756717c77fac
ashencat <forddagujar@gmail.com>
2021-03-31 00:49:14 -0400

    code split the routes

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Login from './pages/Login'
+// import Login from './pages/Login'
 import Home from './pages/Home'
-import Register from './pages/Register'
-import Profile from './pages/Profile'
+// import Register from './pages/Register'
+// import Profile from './pages/Profile'
 import SideNav from './sideNav/SideNav';
 
 export default function Body(props) {
+    const Login = React.lazy(()=>import('./pages/Login'))
+    const Register = React.lazy(()=>import('./pages/Register'))
+    const Profile = React.lazy(()=>import('./pages/Profile'))
+
     return (
         <main style={{flex: 1}}>
             <SideNav open={props.sideNav}  setSideNav={props.setSideNav} />
-            <Route exact path='/Login' component={Login} />
             <Route exact path='/Home' component={Home} />
-            <Route exact path='/Register' component={Register} />
-            <Route exact path='/Profile' component={Profile} />
+            <React.Suspense fallback={<p>Loading...</p>}>
+                <Route exact path='/Login' component={Login} />
+                <Route exact path='/Register' component={Register} />
+                <Route exact path='/Profile' component={Profile} />
+            </React.Suspense>
         </main>
     )
 }
