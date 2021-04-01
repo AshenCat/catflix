0398590393908dd35ff3841970f5cf8d0f8d6e96
ashencat <forddagujar@gmail.com>
2021-04-01 15:31:11 -0400

    snackbar fix

--- src/context/SnackbarContext.js
+++ src/context/SnackbarContext.js
@@ -1,5 +1,7 @@
 import { Snackbar } from '@material-ui/core'
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const SnackbarContext = React.createContext();
 
@@ -12,13 +14,35 @@ function SnackbarProvider(props) {
     const [anchorOrigin, setAnchorOrigin] = React.useState({vertical: 'bottom', horizontal: 'right'})
     const [message, setMessage] = React.useState('')
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+        setOpen(false);
+      };
+
     return (
         <SnackbarContext.Provider value={{setOpen, setMessage, setAnchorOrigin}}>
             {props.children}
             <Snackbar
                 open={open}
                 anchorOrigin={anchorOrigin}
-                message={message}/>
+                message={message}
+                autoHideDuration={2500}
+                onClose={handleClose}
+                action={
+                    <React.Fragment>
+                      <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        onClick={handleClose}
+                        
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </React.Fragment>
+                  }
+                />
         </SnackbarContext.Provider>
     )
 }
