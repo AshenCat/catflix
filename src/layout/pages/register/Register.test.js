ee0f451dce7257ccc278a39f5a9948aa1ed3f6d5
ashencat <forddagujar@gmail.com>
2021-04-03 14:31:32 -0400

    login/register test and ui match

---
+++ src/layout/pages/register/Register.test.js
@@ -0,0 +1,20 @@
+import { RegisterUsernameValidation, RegisterPasswordValidation } from "./Register";
+
+test("username validation", () => {
+    expect(RegisterUsernameValidation('five5')).toBe(false);
+    expect(RegisterUsernameValidation(' six6 ')).toBe(false);
+    expect(RegisterUsernameValidation('')).toBe(false);
+    expect(RegisterUsernameValidation('si 666')).toBe(false);
+    expect(RegisterUsernameValidation('a__ __a')).toBe(false);
+    expect(RegisterUsernameValidation('__six__')).toBe(true);
+    expect(RegisterUsernameValidation('six666')).toBe(true);
+})
+
+test("password validation", () => {
+    expect(RegisterPasswordValidation('.')).toBe(false);
+    expect(RegisterPasswordValidation('')).toBe(false);
+    expect(RegisterPasswordValidation('aaaaa')).toBe(false);
+    expect(RegisterPasswordValidation('aaaaa ')).toBe(false);
+    expect(RegisterPasswordValidation('aaaaa_')).toBe(true);
+    expect(RegisterPasswordValidation('aaaaaa')).toBe(true);
+})
