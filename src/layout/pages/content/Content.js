4443f9ca0c805c09108997b582cbb930ff19e268
ashencat <forddagujar@gmail.com>
2021-04-15 19:09:53 -0400

    now has a delay before onresize rerender

--- src/layout/pages/content/Content.js
+++ src/layout/pages/content/Content.js
@@ -4,22 +4,35 @@ import Column from './column/Column';
 import './content.scss'
 
 function Content() {
-
     const [width, setWidth] = React.useState(window.innerWidth);
-    const updateSize = () => {
-        setWidth(window.innerWidth);
-    }
+    const [perPage] = [5]
     
+    const columns = React.useMemo(() => {
+        return Math.floor(width / 280)
+    },[width]);
+    // const rerender = React.useRef(0)
+    // 
+    let doit;
+    const updateSize = () => {
+        clearTimeout(doit);
+        doit = setTimeout(()=>{
+            console.log('hmmmm')
+            setWidth(window.innerWidth)
+        },100)
+    }
+    // React.useEffect(()=>console.log(++rerender.current), []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     React.useEffect(() => (window.onresize = updateSize), []);
     // Generate coloumn depending on screen width
     const ColumnGenerator = () => {
-        const columns = Math.floor(width / 280);
         console.log("columns : ", columns)
         return (
-            [...new Array(columns > 0 ? columns : 1)].map((unuseable, index)=>
-                <div key={index} className="content-column">
-                    <Column />
-                </div>)
+            [...new Array(columns)].map((unuseable, index)=>
+                {   
+                    return <div key={index} className="content-column">
+                        <Column page={index} perPage={perPage} />
+                    </div>
+                })
             )
     }
 
